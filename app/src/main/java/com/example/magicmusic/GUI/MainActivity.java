7fc27542e314c2c54b8818f162533d3c6bf5d50c
Akira Satoh <akira31758421@gmail.com>
2024-10-26 21:24:09 +0700

    Create GUI

--- app/src/main/java/com/example/magicmusic/GUI/MainActivity.java
+++ app/src/main/java/com/example/magicmusic/GUI/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.magicmusic;
+package com.example.magicmusic.GUI;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.example.magicmusic.R;
 
 public class MainActivity extends AppCompatActivity {
 
