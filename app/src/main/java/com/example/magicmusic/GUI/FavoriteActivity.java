21f057ac3c71f2b58cff839f19cbf68096d2172e
GDevNeON <neongaming4ever@gmail.com>
2024-11-02 15:30:45 +0700

    Long xong gui favorite, song widgets, chưa implement logic

--- app/src/main/java/com/example/magicmusic/GUI/FavoriteActivity.java
+++ app/src/main/java/com/example/magicmusic/GUI/FavoriteActivity.java
@@ -1,13 +1,23 @@
 package com.example.magicmusic.GUI;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.magicmusic.R;
+
 
 public class FavoriteActivity extends AppCompatActivity {
 
@@ -17,10 +27,8 @@ public class FavoriteActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.favorite);
 
-        ScrollView sv = findViewById(R.id.scroll_view);
         TextView svNoContentNotify = findViewById(R.id.scroll_view_notify);
-        LinearLayout contentLayout = findViewById(R.id.scroll_view_container);
-
+        RelativeLayout contentLayout = findViewById(R.id.scroll_view_container);
         if (contentLayout.getChildCount() == 0) {
             svNoContentNotify.setText(R.string.favorite_empty_list);
             svNoContentNotify.setVisibility(View.VISIBLE);
